a73febaf7a018a6d1441918ba2f52528f0896f45
SAHIL080603 <sahil14874@gmail.com>
2021-01-10 18:53:47 +0530

    programs updated

--- pascalstriangle.cpp
+++ pascalstriangle.cpp
@@ -3,15 +3,17 @@ using namespace std;
 int main() {
 	int row;
 	cin>>row;
+
 	for (int n=0; n<=row-1; n++){
 		for (int s=1; s<=(2*row-2)-2*(n-1); s++){
-			cout<<' ';
+			cout<<' ';                    //printing spaces 
 		}
 		
 		if (n==0){
-			cout<<"  "<<1;
+			cout<<"   "<<1;
 		}
 		else{
+			
 			for (int r=0; r<=n; r++){
 				int fn=1, frn=1, fr=1;
 			for (int i=1; i<=n; i++){
@@ -23,8 +25,8 @@ int main() {
 			for (int i=1; i<=r; i++){
 				fr=fr*i;
 			}
-			cout<<"  "<<fn/(frn*fr);
-			cout<<' ';
+			cout<<"   "<<fn/(frn*fr);         //not using the basic concept of triangle
+			// using the general concept of binomial theorem
 			
 		}
 		}
